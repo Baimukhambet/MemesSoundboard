d23835d00ab2872304bc3975b91bfc923cfcf002
Timka <vendetta10090@gmail.com>
2023-07-03 02:13:48 +0600

    mvp

--- Memes/ContentView.swift
+++ Memes/ContentView.swift
@@ -12,28 +12,30 @@ import UIKit
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            VStack(spacing: 0) {
-                MemeCollectionView().background(Color(red: 33/255, green: 33/255, blue: 35/255).opacity(0.8))
-            }
-            .font(.title2)
-            .tabItem {
-                Image(systemName: "house").renderingMode(.template).foregroundColor(.yellow)
-                Text("Main")
-            }
-            
-            Text("Tab 2")
-                .tabItem {
-                    Image(systemName: "moon")
-                    Text("Tab 2")
+        NavigationView {
+            TabView {
+                VStack(spacing: 0) {
+                    MemeCollectionView().background(Color(red: 33/255, green: 33/255, blue: 35/255).opacity(0.8))
                 }
-        }
-        .onAppear {
-            let appearance = UITabBarAppearance()
-            appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
-            appearance.backgroundColor = UIColor(Color.orange.opacity(0.2))
-            UITabBar.appearance().standardAppearance = appearance
-            UITabBar.appearance().scrollEdgeAppearance = appearance
+                .font(.title2)
+                .tabItem {
+                    Image(systemName: "house").renderingMode(.template).foregroundColor(.black)
+                    Text("Main")
+                }
+                
+                Text("Quiz")
+                    .tabItem {
+                        Image(systemName: "arrowtriangle.right.circle").renderingMode(.template).foregroundColor(.black)
+                        Text("Quiz")
+                    }
+            }
+            .onAppear {
+                let appearance = UITabBarAppearance()
+                appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+                appearance.backgroundColor = UIColor(Color.orange.opacity(0.2))
+                UITabBar.appearance().standardAppearance = appearance
+                UITabBar.appearance().scrollEdgeAppearance = appearance
+            }
         }
     }
 }
